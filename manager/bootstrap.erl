d59593b3540bd40cd411ed2e7cfd2cf3193d9641
Alonso Vidales <alonso.vidales@socialpoint.es>
2012-10-06 00:58:53 +0200

    Working on node communication with master

--- manager/bootstrap.erl
+++ manager/bootstrap.erl
@@ -17,20 +17,20 @@
 %% receive during ?TIME_TO_REFRESH_NODE_MS miliseconds, remove the
 %% node from the nodes lists, should to be run on a new process
 %%
-%% @param NodePid pid The pid of the node to check
+%% @param NodePid_addr pid The pid of the node to check
 %%
-check_node_status(NodePid) ->
+check_node_status(Node_pid) ->
     receive
         % Only to know if the node is still alive
-        active ->
+        im_alive ->
             io:format("Refresh active node~n"),
-            check_node_status(NodePid)
+            check_node_status(Node_pid)
 
     % If we don't receive a communication from the node in the
     % lasts TIME_TO_REFRESH_NODE_MS the node is down, remove it
     after ?TIME_TO_REFRESH_NODE_MS ->
-        io:format(standard_error, "Time out on node ~w~n", [NodePid]),
-        manager ! {remove_node, NodePid}
+        io:format(standard_error, "Time out on node ~w~n", [Node_pid]),
+        manager ! {remove_node, Node_pid}
     end.
 
 %%
@@ -41,56 +41,61 @@ check_node_status(NodePid) ->
 %% @param ActiveNodes list List of active nodes
 %%
 update_nodes_lists([Node_to_update | Nodes_not_updated], ActiveNodes) ->
+    io:format("Updating nodes ~w - ~w~n", [Node_to_update, ActiveNodes]),
     Node_to_update ! {update_nodes_list, ActiveNodes},
 
     if
         length(Nodes_not_updated) > 0 ->
-            update_nodes_lists(Nodes_not_updated, ActiveNodes)
-    end.
+            update_nodes_lists(Nodes_not_updated, ActiveNodes);
+        true ->
+            false
+    end;
+update_nodes_lists([], []) ->
+    false.
 
 %%
 %% Listen for the messages to add or remove nodes from the nodes
 %% list, or get the current nodes list
 %%
-start_listener(Is_master, Nodes) ->
+listener_loop(Is_master, Nodes) ->
     io:format("Current Nodes ~w~n", [Nodes]),
 
     receive
-        % Adds a node with pid "Pid" to the list of nodes and starts
+        % Adds a node with pid "Pid_addr" to the list of nodes and starts
         % monitorization
-        {add_node, Pid} ->
+        {add_node, Pid_addr, Check_id} ->
             % Check if the pid is in the list to don't add it twice
-            Yet_in_list = lists:member(Pid, Nodes),
+            Yet_in_list = lists:member(Pid_addr, Nodes),
 
             if
                 not Yet_in_list ->
-                    io:format("Adding node ~w~n", [string:concat("check_", pid_to_list(Pid))]),
+                    io:format("Adding node ~w~n", [list_to_atom(Check_id)]),
                     % Add the supervisor loop, controls that the node is active,
                     % and in case of a down, remove it
                     register(
-                        list_to_atom(string:concat("check_", pid_to_list(Pid))),
-                        spawn(bootstrap, check_node_status, [Pid])),
+                        list_to_atom(Check_id),
+                        spawn(bootstrap, check_node_status, [Pid_addr])),
 
                     % Update the list of nodes on each active node
-                    update_nodes_lists(Nodes, Nodes),
-                    start_listener(Is_master, [Pid | Nodes]);
+                    update_nodes_lists([Pid_addr | Nodes], [Pid_addr | Nodes]),
+                    listener_loop(Is_master, [Pid_addr | Nodes]);
                 true ->
                     io:format("Node yet added ~w~n", [Nodes]),
-                    start_listener(Is_master, Nodes)
+                    listener_loop(Is_master, Nodes)
             end;
 
-        % Remove a node with pid "Pid" from the list of nodes
-        {remove_node, Pid} ->
-            io:format("Removing node node ~w~n", [Pid]),
-            Active_nodes = lists:delete(Pid, Nodes),
+        % Remove a node with pid "Pid_addr" from the list of nodes
+        {remove_node, Pid_addr} ->
+            io:format("Removing node node ~w~n", [Pid_addr]),
+            Active_nodes = lists:delete(Pid_addr, Nodes),
             % Update the list of nodes on each active node
             update_nodes_lists(Active_nodes, Active_nodes),
-            start_listener(Is_master, Active_nodes);
+            listener_loop(Is_master, Active_nodes);
 
         % Sends the list of active nodes to the node
-        {get_nodes, Pid} ->
-            Pid ! Nodes,
-            start_listener(Is_master, Nodes)
+        {get_nodes, Pid_addr} ->
+            Pid_addr ! Nodes,
+            listener_loop(Is_master, Nodes)
     end.
 
 %%
@@ -122,7 +127,7 @@ start() ->
         node() == Node_addr ->
             io:format("Node Name: \"~w\" @ \"~w\"~nStarting listener...~n", [manager, node()]),
             register(manager, self()),
-            start_listener(Is_master, []);
+            listener_loop(Is_master, []);
 
         true ->
             io:format("The configured node address: \"~w\" doesn't correspond with the current address: \"~w\"~n", [Node_addr, node()]),
--- shared/config.hrl
+++ shared/config.hrl
@@ -5,8 +5,8 @@
 %%
 
 %% The nodes will send each time_to_refesh_node_ms miliseconds a signal to keep it alive
--define(TIME_TO_REFRESH_NODE_MS, 5000).
-%% Manager node, will be used as primary
+-define(TIME_TO_REFRESH_NODE_MS, 10000).
+%% Manager master node, will be used as primary
 -define(MASTER_MANAGER, manager@tras2.es).
 %% Slave node
 -define(SLAVE_MANAGER, manager@tras2.es).
